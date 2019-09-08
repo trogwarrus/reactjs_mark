e3e806686870fcd8cd75e2ce277f7b7c7e4ab879
trogwarrus <ihman@mail.ru>
2019-09-09 02:51:16 +0300

    build project

---
+++ build/precache-manifest.64d0d681cbf2ef93533904d426de362b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cf9d909014617e7ea2ed737eb98d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49c093c37b96b8cfb86c",
+    "url": "/static/css/main.cdf8f0ae.chunk.css"
+  },
+  {
+    "revision": "4eb713dadad4634d20a5",
+    "url": "/static/js/2.311451a2.chunk.js"
+  },
+  {
+    "revision": "49c093c37b96b8cfb86c",
+    "url": "/static/js/main.7aa9fd50.chunk.js"
+  },
+  {
+    "revision": "97b175a3414b20c7cd98",
+    "url": "/static/js/runtime~main.8cb3155e.js"
+  }
+]);
